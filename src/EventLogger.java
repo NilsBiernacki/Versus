6b7424074866b24f3f9a0cb49fa62bfbfd74fe12
Nils Biernacki <nils.biernacki@stud.hs-ruhrwest.de>
2017-03-27 00:34:49 +0200

    prepared EventLogger and create Game class

--- src/EventLogger.java
+++ src/EventLogger.java
@@ -7,21 +7,32 @@ public class EventLogger {
 	}
 		
 	void printRoleDiceInfo(Player p) {
-		if (p.getColor() == "ROT") {
+		if (p.getColor().equals("ROT")) {
 			System.out.println("Zeit die Würfel zu rollen. ROT du bist drann, drücke 'w' um zu würfeln.");
 		}
-		else if(p.getColor() == "BLUE") {
+		else if(p.getColor().equals("BLUE")) {
 			System.out.println("OK BLAU, nun bist du am Zug. Viel Glück beim Würfeln. Drücke 'w' um zu würfeln.");
 		}
 	}
 	
 	void printPlayersDiceValue(Player p, int diceValue) {
-		if (p.getColor() == "ROT") {
-			System.out.println("Hey ROT, du haste eine "+ diceValue + " gewürfelt.");
+		if (p.getColor().equals("ROT")) {
+			System.out.println("Hey ROT, du haste eine " + diceValue + " gewürfelt.");
 		}
-		if (p.getColor() == "BLAU") {
-			System.out
+		if (p.getColor().equals("BLAU")) {
+			System.out.println("BLAU, du hast die " + diceValue + " gewürfelt.");
 		}
 	}
+        
+        void printPlayersAction(Player p) {
+            if (p.getColor().equals("ROT")) {
+                        System.out.println("Hey ROT, es wird Zeit BLAU eine Abreibung zu verpassen!\n"
+                                + "Drücke 's' um zu schlagen.");
+		}
+            if (p.getColor().equals("BLAU")) {
+			System.out.println("BLAU, du darfst ROT eine verpassen. Und halt dich bloß nicht zurück.\n"
+                                + "Drücke 's' um zu schlagen.");
+                }
+        }
 	
 }
---
+++ src/Game.java
@@ -0,0 +1,24 @@
+
+public class Game {
+    
+    private Player red;
+    private Player blue;
+    
+    public Game() {
+    
+    }
+    
+    private void init() {
+        this.blue = new Player();
+        this.red  = new Player();
+    }
+    
+    private void run() {
+        /**
+         * This function will run the game. 
+         * Normally, all phases will run and repeat.
+         * If the user wants to close this function returns.
+         */
+    }
+    
+}
